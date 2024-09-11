3c9cf5bd35119494818037c20d32624b82150a77
Emremefa1 <emrecelikler01@hotmail.com>
2024-09-11 22:16:18 +0300

    Finished website

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+// Function to handle clicks outside the details panel
+function handleOutsideClick(event, detailsElement) {
+  if (!detailsElement.contains(event.target)) {
+    detailsElement.classList.remove('active'); // Hide details if clicked outside
+    document.removeEventListener('click', handleOutsideClick); // Remove the listener
+  }
+}
+
+
